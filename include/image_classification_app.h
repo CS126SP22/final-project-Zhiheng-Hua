8e803bb5db0cba6f07e683556ee28504388eb54f
Zhiheng Hua <hua13@illinois.edu>
2022-04-22 17:40:35 -0500

    working UI

--- include/image_classification_app.h
+++ include/image_classification_app.h
@@ -3,8 +3,16 @@
 #include "cinder/app/App.h"
 #include "cinder/app/RendererGl.h"
 #include "cinder/gl/gl.h"
+#include <Eigen/Dense>
+#include "util.h"
 
-
+using ci::gl::Texture2d;
+using glm::vec2;
+using Eigen::MatrixXf;
+using ci::ColorT;
+using std::string;
+using ci::gl::Texture2dRef;
+using ci::Font;
 
 /**
  * An app for image classification
@@ -12,15 +20,19 @@
 class ImageClassificationApp : public ci::app::App {
   public:
     ImageClassificationApp();
-
+    
     void draw() override;
-    void update() override;
+    void fileDrop( ci::app::FileDropEvent event ) override;
 
     // provided that you can see the entire UI on your screen.
-    const int kWindowSizeX = 1000;
-    const int kWindowSizeY = 800;
-    const int kMargin = 100;
+    const int kWindowSizeX = 600;
+    const int kWindowSizeY = 600;
 
   private:
+    Texture2dRef mTex;
+    string image_path_;
+    string message_;
+    string prediction_;
+    ColorT<float> message_color_;
 };
 
--- src/image_classification_app.cc
+++ src/image_classification_app.cc
@@ -1,18 +1,48 @@
 #include "image_classification_app.h"
 
 
-
-ImageClassificationApp::ImageClassificationApp() {
+ImageClassificationApp::ImageClassificationApp() 
+    : image_path_(""), message_(""), message_color_(ColorT<float>("white")), prediction_("")
+{
   ci::app::setWindowSize(kWindowSizeX, kWindowSizeY);
 }
 
 void ImageClassificationApp::draw() {
-  ci::Color background_color("black");
-  ci::gl::clear(background_color);
-}
-
-void ImageClassificationApp::update() {
+  ci::gl::clear(ci::Color("black"));
   
+  if (image_path_ != "") {
+    //"data/CARS/AUDI/download.jpg"
+    auto img = ci::loadImage( image_path_ );
+    mTex = Texture2d::create( img );
+
+    ci::gl::pushModelMatrix();
+    ci::gl::translate( 0.5f * getWindowWidth(), 0.5f * getWindowHeight() );
+    ci::gl::scale( 2.0f, 2.0f );
+    ci::gl::translate( -0.5f * mTex->getWidth(), -0.5f * mTex->getHeight() );
+    ci::gl::draw( mTex );
+    ci::gl::popModelMatrix();
+  }
+
+  Font font = Font("Roboto", 30);
+  ci::gl::drawString(prediction_, vec2(200, 20), ColorT<float>("white"), Font("Roboto", 40));
+  ci::gl::drawString("press space to input image path", vec2(120, 520), ColorT<float>("white"), font);
+  ci::gl::drawString("press return to make prediction", vec2(120, 540), ColorT<float>("white"), font);
+  ci::gl::drawString(message_, vec2(120, 560), message_color_, font);
 }
 
+void ImageClassificationApp::fileDrop( ci::app::FileDropEvent event ) {
+  const auto& file_vec = event.getFiles();
+  
+  if (file_vec.size() != 1) {
+    message_ = "you can only drop one file";
+    message_color_ = ColorT<float>("red");
+    return;
+  }
 
+  image_path_ = "data/" + file_vec[0].filename().string();
+  message_ = "image file: " + image_path_;
+  message_color_ = ColorT<float>("green");
+
+  // TODO: make a Prediction
+  prediction_ = "Prediction";
+}
